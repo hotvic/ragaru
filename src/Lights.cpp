1bc9db850b7f4f89205734e33e81767e68bf2deb
Victor A <victoraur.santos@gmail.com>
2016-06-18 14:31:48 -0300

    remove unused vairables from Lights.cpp

--- src/Lights.cpp
+++ src/Lights.cpp
@@ -24,9 +24,6 @@
 void SetUpLight(Light* whichsource, int whichlight)
 {
     static float qattenuation[] = { 0.0002f };
-    static float cattenuation[] = { 1.5f };
-    static float lattenuation[] = { 0.5f };
-    static float zattenuation[] = { 0.0f };
 
     // Initialize lights
     if(whichlight == 0) {
